27c13e2745ee5618034158088161ae64dbe1fba3
lolaasma98-maker <lolaasma98@gmail.com>
2026-01-22 11:44:28 +0100

    prompt

--- prompt.py
+++ prompt.py
@@ -8,6 +8,15 @@ Joueur actuel: {player}
 Grille:
 {grid}
 
+Règles strictes :
+1. Si vous pouvez aligner 5 maintenant, faites-le immédiatement.
+2. Sinon, bloquez si l'adversaire peut aligner 5 au prochain tour.
+3. Sinon, choisissez le meilleur coup stratégique pour progresser vers 5.
+4. Réponds avec un seul coup sous la forme: r,c (ligne,colonne)
+5. Ne joue qu’à ton tour. Si ce n’est pas ton tour, ne fais rien.
+
+Ne fais jamais plus d'un coup à la fois.
 Réponds avec un seul coup. Format idéal: Coup: r,c
 (mais tu peux aussi répondre: r,c)
+
 """
